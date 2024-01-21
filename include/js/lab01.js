95d79e6f9990071b34923362a6b28a4062857776
Matthew <c0536745@online.camosun.ca>
2024-01-21 15:46:40 -0800

    lab01_v1.0

---
+++ include/js/lab01.js
@@ -0,0 +1,3 @@
+document.getElementById("hello").innerHTML = "Hello, Joe Nelson, How many Rooms do you want to book?!";
+let name = prompt("What's your name?")
+alert(`Hello, ` + name + `. Nice to meet you again.`)
